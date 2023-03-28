334a31ab91fa94d281aaac4aac8547e30c8e0059
lixz <lixz@dns.com>
2023-03-28 20:34:21 +0800

    feat(createUDPListeners): SO_REUSEADDR,SO_REUSEPORT

--- proxy/server_udp.go
+++ proxy/server_udp.go
@@ -1,42 +1,97 @@
 package proxy
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"runtime"
+	"syscall"
 
 	"github.com/AdguardTeam/dnsproxy/proxyutil"
 	"github.com/AdguardTeam/golibs/errors"
 	"github.com/AdguardTeam/golibs/log"
 	"github.com/miekg/dns"
+	"golang.org/x/sys/unix"
 )
 
 func (p *Proxy) createUDPListeners() error {
-	for _, a := range p.UDPListenAddr {
+	n := runtime.NumCPU()
+	if n > 1 {
+		n--
+	}
+
+	reuselisten := func(a *net.UDPAddr) error {
 		udpListen, err := p.udpCreate(a)
 		if err != nil {
 			return err
 		}
 		p.udpListen = append(p.udpListen, udpListen)
+		return nil
+	}
+
+	for _, a := range p.UDPListenAddr {
+		for i := 0; i < n; i++ {
+			err := reuselisten(a)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
 }
 
+var lc = net.ListenConfig{
+	Control: func(network string, address string, c syscall.RawConn) error {
+		var err error
+		c.Control(func(fd uintptr) {
+			err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+			if err != nil {
+				return
+			}
+			err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+			if err != nil {
+				return
+			}
+		})
+		return err
+	},
+}
+
 // udpCreate - create a UDP listening socket
 func (p *Proxy) udpCreate(udpAddr *net.UDPAddr) (*net.UDPConn, error) {
 	log.Info("Creating the UDP server socket")
-	udpListen, err := net.ListenUDP("udp", udpAddr)
+	pc, err := lc.ListenPacket(context.Background(), "udp", udpAddr.String())
 	if err != nil {
 		return nil, fmt.Errorf("listening to udp socket: %w", err)
 	}
-
+	udpListen, ok := pc.(*net.UDPConn)
+	if !ok {
+		return nil, fmt.Errorf("listening to udp socket: %w", err)
+	}
 	if p.Config.UDPBufferSize > 0 {
 		err = udpListen.SetReadBuffer(p.Config.UDPBufferSize)
 		if err != nil {
 			_ = udpListen.Close()
-
-			return nil, fmt.Errorf("setting udp buf size: %w", err)
+			return nil, fmt.Errorf("setting read udp buf size: %w", err)
 		}
+		err = udpListen.SetWriteBuffer(p.Config.UDPBufferSize)
+		if err != nil {
+			_ = udpListen.Close()
+			return nil, fmt.Errorf("setting write udp buf size: %w", err)
+		}
+	}
+	if c, err := udpListen.SyscallConn(); err == nil {
+		c.Control(func(fd uintptr) {
+			bufsize, err := syscall.GetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF)
+			if err == nil {
+				log.Info("UDP socket read buffer size: %d", bufsize)
+			}
+			bufsize, err = syscall.GetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF)
+			if err == nil {
+				log.Info("UDP socket write buffer size: %d", bufsize)
+			}
+		})
 	}
 
 	err = proxyutil.UDPSetOptions(udpListen)
@@ -69,11 +124,14 @@ func (p *Proxy) udpPacketLoop(conn *net.UDPConn, requestGoroutinesSema semaphore
 		if n > 0 {
 			// make a copy of all bytes because ReadFrom() will overwrite contents of b on next call
 			// we need the contents to survive the call because we're handling them in goroutine
-			packet := make([]byte, n)
-			copy(packet, b)
+
+			packetPtr := p.bytesPool.Get().(*[]byte)
+			packet := *packetPtr
+			copy(packet[:n], b[:n])
 			requestGoroutinesSema.acquire()
 			go func() {
-				p.udpHandlePacket(packet, localIP, remoteAddr, conn)
+				p.udpHandlePacket(packet[:n], localIP, remoteAddr, conn)
+				p.bytesPool.Put(packetPtr)
 				requestGoroutinesSema.release()
 			}()
 		}
@@ -128,6 +186,7 @@ func (p *Proxy) respondUDP(d *DNSContext) error {
 
 	conn := d.Conn.(*net.UDPConn)
 	rAddr := d.Addr.(*net.UDPAddr)
+
 	n, err := proxyutil.UDPWrite(bytes, conn, rAddr, d.localIP)
 	if err != nil {
 		if errors.Is(err, net.ErrClosed) {
